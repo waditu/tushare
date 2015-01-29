ab8a52dc86c2c12d29aa0224697b77c2d8d22735
jimmysoa <jimmysoa@sina.cn>
2015-01-29 16:17:29 +0800

    added obtaining macro economy data function

---
+++ tushare/stock/macro.py
@@ -0,0 +1,171 @@
+# -*- coding:utf-8 -*- 
+
+"""
+宏观经济数据接口 
+Created on 2015/01/24
+@author: Jimmy Liu
+@group : waditu
+@contact: jimmysoa@sina.cn
+"""
+
+import macro_vars as vs
+import pandas as pd
+import numpy as np
+import urllib2
+import re
+import json
+
+def get_gdp_year():
+    """
+        获取年度国内生产总值数据
+    Return
+    --------
+    DataFrame
+        year :统计年度
+        gdp :国内生产总值(亿元)
+        pc_gdp :人均国内生产总值(元)
+        gnp :国民生产总值(亿元)
+        pi :第一产业(亿元)
+        si :第二产业(亿元)
+        industry :工业(亿元)
+        cons_industry :建筑业(亿元)
+        ti :第三产业(亿元)
+        trans_industry :交通运输仓储邮电通信业(亿元)
+        lbdy :批发零售贸易及餐饮业(亿元)
+    """
+    rdint = vs.random()
+    url = vs.NATION_URL%(vs.P_TYPE['http'],vs.DOMAINS['sina'],rdint,0,rdint)
+    request = urllib2.Request(url)
+    text = urllib2.urlopen(request,timeout=10).read()
+
+    regSym = re.compile(r'\,count:(.*?)\}')
+    datastr = regSym.findall(text)
+    datastr = datastr[0]
+    datastr = datastr.split('data:')[1]
+    datastr = datastr.replace('"','').replace('null','0')
+    js = json.loads(datastr)
+    df = pd.DataFrame(js,columns=vs.GDP_YEAR_COLS)
+    df[df==0] = np.NaN
+    return df
+    
+def get_gdp_quarter():
+    """
+        获取季度国内生产总值数据
+    Return
+    --------
+    DataFrame
+        quarter :季度
+        gdp :国内生产总值(亿元)
+        gdp_yoy :国内生产总值同比增长(%)
+        pi :第一产业增加值(亿元)
+        pi_yoy:第一产业增加值同比增长(%)
+        si :第二产业增加值(亿元)
+        si_yoy :第二产业增加值同比增长(%)
+        ti :第三产业增加值(亿元)
+        ti_yoy :第三产业增加值同比增长(%)
+    """
+    rdint = vs.random()
+    url = vs.NATION_URL%(vs.P_TYPE['http'],vs.DOMAINS['sina'],rdint,1,rdint)
+    request = urllib2.Request(url)
+    text = urllib2.urlopen(request,timeout=10).read()
+
+    regSym = re.compile(r'\,count:(.*?)\}')
+    datastr = regSym.findall(text)
+    datastr = datastr[0]
+    datastr = datastr.split('data:')[1]
+    datastr = datastr.replace('"','').replace('null','0')
+    js = json.loads(datastr)
+    df = pd.DataFrame(js,columns=vs.GDP_QUARTER_COLS)
+    df['quarter'] = df['quarter'].astype(object)
+    df[df==0] = np.NaN
+    return df
+
+
+def get_gdp_for():
+    """
+        获取三大需求对GDP贡献数据
+    Return
+    --------
+    DataFrame
+        year :统计年度
+        end_for :最终消费支出贡献率(%)
+        for_rate :最终消费支出拉动(百分点)
+        asset_for :资本形成总额贡献率(%)
+        asset_rate:资本形成总额拉动(百分点)
+        goods_for :货物和服务净出口贡献率(%)
+        goods_rate :货物和服务净出口拉动(百分点)
+    """
+    rdint = vs.random()
+    url = vs.NATION_URL%(vs.P_TYPE['http'],vs.DOMAINS['sina'],rdint,4,rdint)
+    request = urllib2.Request(url)
+    text = urllib2.urlopen(request,timeout=10).read()
+
+    regSym = re.compile(r'\,count:(.*?)\}')
+    datastr = regSym.findall(text)
+    datastr = datastr[0]
+    datastr = datastr.split('data:')[1]
+    datastr = datastr.replace('"','').replace('null','0')
+    js = json.loads(datastr)
+    df = pd.DataFrame(js,columns=vs.GDP_FOR_COLS)
+    df[df==0] = np.NaN
+    return df
+
+def get_gdp_pull():
+    """
+        获取三大产业对GDP拉动数据
+    Return
+    --------
+    DataFrame
+        year :统计年度
+        gdp_yoy :国内生产总值同比增长(%)
+        pi :第一产业拉动率(%)
+        si :第二产业拉动率(%)
+        industry:其中工业拉动(%)
+        ti :第三产业拉动率(%)
+    """
+    rdint = vs.random()
+    url = vs.NATION_URL%(vs.P_TYPE['http'],vs.DOMAINS['sina'],rdint,5,rdint)
+    request = urllib2.Request(url)
+    text = urllib2.urlopen(request,timeout=10).read()
+
+    regSym = re.compile(r'\,count:(.*?)\}')
+    datastr = regSym.findall(text)
+    datastr = datastr[0]
+    datastr = datastr.split('data:')[1]
+    datastr = datastr.replace('"','').replace('null','0')
+    js = json.loads(datastr)
+    df = pd.DataFrame(js,columns=vs.GDP_PULL_COLS)
+    df[df==0] = np.NaN
+    return df
+
+def get_gdp_contrib():
+    """
+        获取三大产业贡献率数据
+    Return
+    --------
+    DataFrame
+        year :统计年度
+        gdp_yoy :国内生产总值
+        pi :第一产业献率(%)
+        si :第二产业献率(%)
+        industry:其中工业献率(%)
+        ti :第三产业献率(%)
+    """
+    rdint = vs.random()
+    url = vs.NATION_URL%(vs.P_TYPE['http'],vs.DOMAINS['sina'],rdint,6,rdint)
+    request = urllib2.Request(url)
+    text = urllib2.urlopen(request,timeout=10).read()
+
+    regSym = re.compile(r'\,count:(.*?)\}')
+    datastr = regSym.findall(text)
+    datastr = datastr[0]
+    datastr = datastr.split('data:')[1]
+    datastr = datastr.replace('"','').replace('null','0')
+    js = json.loads(datastr)
+    df = pd.DataFrame(js,columns=vs.GDP_CONTRIB_COLS)
+    df[df==0] = np.NaN
+    return df
+
+if __name__ == '__main__':
+    print get_gdp_contrib()
+
---
+++ tushare/stock/macro_vars.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+
+P_TYPE = {'http':'http://','ftp':'ftp://'}
+DOMAINS = {'sina':'sina.com.cn','sinahq':'sinajs.cn','ifeng':'ifeng.com'}
+NATION_URL = '%smoney.finance.%s/mac/api/jsonp.php/SINAREMOTECALLCALLBACK%s/MacPage_Service.get_pagedata?cate=nation&event=%s&from=0&num=250&condition=&_=%s'
+GDP_YEAR_COLS = ['year','gdp','pc_gdp','gnp','pi','si','industry','cons_industry','ti','trans_industry','lbdy']
+GDP_QUARTER_COLS = ['quarter','gdp','gdp_yoy','pi','pi_yoy','si','si_yoy','ti','ti_yoy']
+GDP_FOR_COLS = ['year','end_for','for_rate','asset_for','asset_rate','goods_for','goods_rate']
+GDP_PULL_COLS = ['year','gdp_yoy','pi','si','industry','ti']
+GDP_CONTRIB_COLS = ['year','gdp_yoy','pi','si','industry','ti']
+
+def random(n=13):
+    from random import randint
+    start = 10**(n-1)
+    end = (10**n)-1
+    return str(randint(start, end))
