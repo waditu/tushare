2b523c303afc4825d5621ab22b5ec7a828fe4d5c
jimmysoa <jimmysoa@sina.cn>
2015-02-28 11:05:58 +0800

    added news content obtaining

--- tushare/stock/news_vars.py
+++ tushare/stock/news_vars.py
@@ -2,3 +2,4 @@
 
 LATEST_URL = '%sroll.news.%s/interface/%s?col=43&spec=&type=&ch=03&k=&offset_page=0&offset_num=0&num=%s&asc=&page=1&r=0.%s'
 LATEST_COLS = ['classify','title','time','url']
+LATEST_COLS_C = ['classify','title','time','url','content']
--- tushare/stock/newsevent.py
+++ tushare/stock/newsevent.py
@@ -14,23 +14,30 @@ from tushare.stock import news_vars as nv
 import demjson
 import pandas as pd
 from datetime import datetime
+import lxml.html
+from lxml import etree
 
-
-def get_latest_news():
+def get_latest_news(top=ct.PAGE_NUM[2], show_content=False):
     """
         获取即时财经新闻
+    
+    Parameters
+    --------
+        top:数值，显示最新消息的条数，默认为80条
+        show_content:是否显示新闻内容，默认False
+    
     Return
     --------
-    DataFrame
-        classify :新闻类别
-        title :新闻标题
-        time :发布时间
-        url :新闻链接
+        DataFrame
+            classify :新闻类别
+            title :新闻标题
+            time :发布时间
+            url :新闻链接
     """
     try:
         request = urllib2.Request(nv.LATEST_URL % (ct.P_TYPE['http'], ct.DOMAINS['sina'],
                                                    ct.PAGES[
-                                                       'lnews'], ct.PAGE_NUM[2],
+                                                       'lnews'],top ,
                                                    _random()))
         data_str = urllib2.urlopen(request, timeout=10).read()
         data_str = data_str.decode('GBK')
@@ -41,12 +48,37 @@ def get_latest_news():
         for r in data_str:
             rt = datetime.fromtimestamp(r['time'])
             rtstr = datetime.strftime(rt, "%m-%d %H:%M")
-            data.append([r['channel']['title'], r['title'], rtstr, r['url']])
-        df = pd.DataFrame(data, columns=nv.LATEST_COLS)
+            arow = [r['channel']['title'], r['title'], rtstr, r['url']]
+            if show_content:
+                arow.append(latest_content(r['url']))
+            data.append(arow)
+        df = pd.DataFrame(data, columns=nv.LATEST_COLS_C if show_content else nv.LATEST_COLS)
         return df
     except Exception as er:
         print str(er)
 
+
+def latest_content(url):
+    '''
+        获取即时财经新闻内容
+    Parameter
+    --------
+        url:新闻链接
+    
+    Return
+    --------
+        string:返回新闻的文字内容
+    '''
+    try:
+        html = lxml.html.parse(url)
+        res = html.xpath('//div[@id=\"artibody\"]/p')
+        sarr = [etree.tostring(node) for node in res]
+        sarr = ''.join(sarr).replace('&#12288;', '')#.replace('\n\n', '\n').
+        html_content = lxml.html.fromstring(sarr)
+        content = html_content.text_content()
+        return content
+    except Exception as er:
+        print str(er)  
 
 def _random(n=16):
     from random import randint
