ffc27c50eb8bdaf72dafe1cdaa6d377cc8f06547
jimmysoa <jimmysoa@sina.cn>
2015-03-09 21:48:55 +0800

    updated init file

--- tushare/__init__.py
+++ tushare/__init__.py
@@ -4,16 +4,19 @@ __author__ = 'Jimmy Liu'
 
 from tushare.stock.trading import (get_hist_data, get_tick_data,
                                    get_today_all, get_realtime_quotes)
+
 from tushare.stock.fundamental import (get_stock_basics, get_report_data,
                                        get_forecast_data, get_profit_data,
                                        get_operation_data, get_growth_data,
                                        get_debtpaying_data, get_cashflow_data)
+
 from tushare.stock.macro import (get_gdp_year, get_gdp_quarter,
                                  get_gdp_for, get_gdp_pull,
                                  get_gdp_contrib, get_cpi,
                                  get_ppi, get_deposit_rate,
                                  get_loan_rate, get_rrr,
                                  get_money_supply, get_money_supply_bal)
+
 from tushare.stock.classifying import (get_industry_classified, get_concept_classified,
                                        get_area_classified, get_gem_classified,
                                        get_sme_classified, get_st_classified,
