bc2f185f868538863a1eb4b1d50f3bb66e51c5a1
jimmysoa@sina.cn <jimmysoa@sina.cn>
2015-06-22 22:48:48 +0800

    bug fixed

--- tushare/stock/trading.py
+++ tushare/stock/trading.py
@@ -546,4 +546,4 @@ def _code_to_symbol(code):
         if len(code) != 6 :
             return ''
         else:
-            return 'sh%s'%code if code[:1] in ['5,', '6'] else 'sz%s'%code
+            return 'sh%s'%code if code[:1] in ['5', '6'] else 'sz%s'%code
