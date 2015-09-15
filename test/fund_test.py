b46a20f2220683a0f2cd171d2ecbdf7e06611e03
Jimmy <zhiming.liu@datayes.com>
2015-09-15 08:44:06 +0800

    test case

--- test/fund_test.py
+++ test/fund_test.py
@@ -15,29 +15,29 @@ class Test(unittest.TestCase):
     def test_get_stock_basics(self):
         print(fd.get_stock_basics())
         
-    def test_get_report_data(self):
-        self.set_data()
-        print(fd.get_report_data(self.year, self.quarter))
-    
-    def test_get_profit_data(self):
-        self.set_data()
-        print(fd.get_profit_data(self.year, self.quarter))
-        
-    def test_get_operation_data(self):
-        self.set_data()
-        print(fd.get_operation_data(self.year, self.quarter))
-        
-    def test_get_growth_data(self):
-        self.set_data()
-        print(fd.get_growth_data(self.year, self.quarter))
-        
-    def test_get_debtpaying_data(self):
-        self.set_data()
-        print(fd.get_debtpaying_data(self.year, self.quarter))
-        
-    def test_get_cashflow_data(self):
-        self.set_data()
-        print(fd.get_cashflow_data(self.year, self.quarter))
+#     def test_get_report_data(self):
+#         self.set_data()
+#         print(fd.get_report_data(self.year, self.quarter))
+#     
+#     def test_get_profit_data(self):
+#         self.set_data()
+#         print(fd.get_profit_data(self.year, self.quarter))
+#         
+#     def test_get_operation_data(self):
+#         self.set_data()
+#         print(fd.get_operation_data(self.year, self.quarter))
+#         
+#     def test_get_growth_data(self):
+#         self.set_data()
+#         print(fd.get_growth_data(self.year, self.quarter))
+#         
+#     def test_get_debtpaying_data(self):
+#         self.set_data()
+#         print(fd.get_debtpaying_data(self.year, self.quarter))
+#         
+#     def test_get_cashflow_data(self):
+#         self.set_data()
+#         print(fd.get_cashflow_data(self.year, self.quarter))
 
 if __name__ == '__main__':
     unittest.main()
