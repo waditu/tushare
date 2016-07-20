d66d01a68aee32f30b74de9b49d9ddfc3a222f39
afay <elliot@ElliotMacBook-Pro.local>
2016-07-20 17:34:00 +0800

    修改fundamental.py中的函数，增加排序功能
    修改fundamental.py中的函数，增加排序功能

--- tushare/stock/cons.py
+++ tushare/stock/cons.py
@@ -12,30 +12,30 @@ K_MIN_LABELS = ['5', '15', '30', '60']
 K_TYPE = {'D': 'akdaily', 'W': 'akweekly', 'M': 'akmonthly'}
 INDEX_LABELS = ['sh', 'sz', 'hs300', 'sz50', 'cyb', 'zxb', 'zx300', 'zh500']
 INDEX_LIST = {'sh': 'sh000001', 'sz': 'sz399001', 'hs300': 'sz399300',
-              'sz50': 'sh000016', 'zxb': 'sz399005', 'cyb': 'sz399006', 'zx300': 'sz399008', 'zh500':'sh000905'}
+              'sz50': 'sh000016', 'zxb': 'sz399005', 'cyb': 'sz399006', 'zx300': 'sz399008', 'zh500': 'sh000905'}
 P_TYPE = {'http': 'http://', 'ftp': 'ftp://'}
 PAGE_NUM = [38, 60, 80, 100]
 FORMAT = lambda x: '%.2f' % x
 FORMAT4 = lambda x: '%.4f' % x
 DOMAINS = {'sina': 'sina.com.cn', 'sinahq': 'sinajs.cn',
            'ifeng': 'ifeng.com', 'sf': 'finance.sina.com.cn',
-           'vsf': 'vip.stock.finance.sina.com.cn', 
+           'vsf': 'vip.stock.finance.sina.com.cn',
            'idx': 'www.csindex.com.cn', '163': 'money.163.com',
            'em': 'eastmoney.com', 'sseq': 'query.sse.com.cn',
            'sse': 'www.sse.com.cn', 'szse': 'www.szse.cn',
-           'oss': '218.244.146.57', 'idxip':'115.29.204.48',
-           'shibor': 'www.shibor.org', 'mbox':'www.cbooo.cn'}
+           'oss': '218.244.146.57', 'idxip': '115.29.204.48',
+           'shibor': 'www.shibor.org', 'mbox': 'www.cbooo.cn'}
 PAGES = {'fd': 'index.phtml', 'dl': 'downxls.php', 'jv': 'json_v2.php',
-         'cpt': 'newFLJK.php', 'ids': 'newSinaHy.php', 'lnews':'rollnews_ch_out_interface.php',
-         'ntinfo':'vCB_BulletinGather.php', 'hs300b':'000300cons.xls',
-         'hs300w':'000300closeweight.xls','sz50b':'000016cons.xls',
-         'dp':'all_fpya.php', '163dp':'fpyg.html',
-         'emxsg':'JS.aspx', '163fh':'jjcgph.php',
-         'newstock':'vRPD_NewStockIssue.php', 'zz500b':'000905cons.xls',
-         'zz500wt':'000905closeweight.xls',
-         't_ticks':'vMS_tradedetail.php', 'dw': 'downLoad.html',
-         'qmd':'queryMargin.do', 'szsefc':'ShowReport.szse',
-         'ssecq':'commonQuery.do', 'sinadd':'cn_bill_download.php', 'ids_sw':'SwHy.php'}
+         'cpt': 'newFLJK.php', 'ids': 'newSinaHy.php', 'lnews': 'rollnews_ch_out_interface.php',
+         'ntinfo': 'vCB_BulletinGather.php', 'hs300b': '000300cons.xls',
+         'hs300w': '000300closeweight.xls', 'sz50b': '000016cons.xls',
+         'dp': 'all_fpya.php', '163dp': 'fpyg.html',
+         'emxsg': 'JS.aspx', '163fh': 'jjcgph.php',
+         'newstock': 'vRPD_NewStockIssue.php', 'zz500b': '000905cons.xls',
+         'zz500wt': '000905closeweight.xls',
+         't_ticks': 'vMS_tradedetail.php', 'dw': 'downLoad.html',
+         'qmd': 'queryMargin.do', 'szsefc': 'ShowReport.szse',
+         'ssecq': 'commonQuery.do', 'sinadd': 'cn_bill_download.php', 'ids_sw': 'SwHy.php'}
 TICK_COLUMNS = ['time', 'price', 'change', 'volume', 'amount', 'type']
 TODAY_TICK_COLUMNS = ['time', 'price', 'pchange', 'change', 'volume', 'amount', 'type']
 DAY_TRADING_COLUMNS = ['code', 'symbol', 'name', 'changepercent',
@@ -60,10 +60,11 @@ INX_DAY_PRICE_COLUMNS = ['date', 'open', 'high', 'close', 'low', 'volume', 'pric
 LIVE_DATA_COLS = ['name', 'open', 'pre_close', 'price', 'high', 'low', 'bid', 'ask', 'volume', 'amount',
                   'b1_v', 'b1_p', 'b2_v', 'b2_p', 'b3_v', 'b3_p', 'b4_v', 'b4_p', 'b5_v', 'b5_p',
                   'a1_v', 'a1_p', 'a2_v', 'a2_p', 'a3_v', 'a3_p', 'a4_v', 'a4_p', 'a5_v', 'a5_p', 'date', 'time', 's']
-FOR_CLASSIFY_B_COLS = ['code','name']
-FOR_CLASSIFY_W_COLS = ['date','code', 'weight']
-FOR_CLASSIFY_W5_COLS = ['date','code', 'name', 'weight']
-THE_FIELDS = ['code','symbol','name','changepercent','trade','open','high','low','settlement','volume','turnoverratio']
+FOR_CLASSIFY_B_COLS = ['code', 'name']
+FOR_CLASSIFY_W_COLS = ['date', 'code', 'weight']
+FOR_CLASSIFY_W5_COLS = ['date', 'code', 'name', 'weight']
+THE_FIELDS = ['code', 'symbol', 'name', 'changepercent', 'trade', 'open', 'high', 'low', 'settlement', 'volume',
+              'turnoverratio']
 TICK_PRICE_URL = '%smarket.%s/%s?date=%s&symbol=%s'
 TODAY_TICKS_PAGE_URL = '%s%s/quotes_service/api/%s/CN_Transactions.getAllPageTime?date=%s&symbol=%s'
 TODAY_TICKS_URL = '%s%s/quotes_service/view/%s?symbol=%s&date=%s&page=%s'
@@ -71,18 +72,24 @@ DAY_PRICE_URL = '%sapi.finance.%s/%s/?code=%s&type=last'
 LIVE_DATA_URL = '%shq.%s/rn=%s&list=%s'
 DAY_PRICE_MIN_URL = '%sapi.finance.%s/akmin?scode=%s&type=%s'
 SINA_DAY_PRICE_URL = '%s%s/quotes_service/api/%s/Market_Center.getHQNodeData?num=80&sort=changepercent&asc=0&node=hs_a&symbol=&_s_r_a=page&page=%s'
-REPORT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/mainindex/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
+REPORT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/mainindex/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C1'   # 增加排序功能 &order=%s%%7C1
+REPORT_ORDERBY= {'default':'code','code':'code', 'eps':'eps','bvps':'bvps', 'roe':'roe', 'netprofit':'netprofit','net_profit':'netprofit','net_profits':'netprofit','cps':'cps','epcf':'cps'}  # 排序检查用
 FORECAST_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/performance/%s?s_i=&s_a=&s_c=&s_type=&reportdate=%s&quarter=%s&p=%s&num=%s'
-PROFIT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/profit/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-OPERATION_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/operation/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-GROWTH_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/grow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-DEBTPAYING_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/debtpaying/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-CASHFLOW_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/cashflow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-SHIBOR_TYPE ={'Shibor': 'Shibor数据', 'Quote': '报价数据', 'Tendency': 'Shibor均值数据',
-              'LPR': 'LPR数据', 'LPR_Tendency': 'LPR均值数据'}
+PROFIT_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/profit/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C1'   # 增加排序功能 &order=%s%%7C1
+PROFIT_ORDERBY = {'default':'roe','roe':'roe', 'netprofitmargin':'netprofitmargin','net_profit_ratio':'netprofitmargin','profitmargin':'profitmargin','gross_profit_rate':'profitmargin','netprofit':'netprofit','net_profits':'netprofit','eps':'eps','business_income':'income', 'income':'income','mips':'mips','bips':'mips'}   # 排序检查用
+OPERATION_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/operation/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C2' # 增加排序功能 &order=%s%%7C2
+OPERATION_ORDERBY = {'default':'yszkzzl','yszkzzl':'yszkzzl','arturnover':'yszkzzl','yszkzzts':'yszkzzts','arturndays':'yszkzzts','chzzl':'chzzl','inventory_turnover':'chzzl','chzzts':'chzzts ','inventory_days':'chzzts','ldzczzl':'ldzczzl','urrentasset_turnover':'ldzczzl','ldzczzts':'ldzczzts','currentasset_days':'ldzczzts'}   # 排序检查用
+GROWTH_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/grow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C2'   # 增加排序功能 &order=%s%%7C1
+GROWTH_ORDERBY = {'default':'mainincome','mainincome':'mainincome','mbrg':'mainincome','netprofit':'netprofit','nprg':'netprofit','netassets':'netassets','nav':'netassets','totalassets':'totalassets','targ':'totalassets'}  # 排序检查用
+DEBTPAYING_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/debtpaying/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C2'  # 增加排序功能 &order=%s%%7C2
+DEBTPAYING_ORDERBY = {'default':'current', 'current':'current','currentratio':'current','quick':'quick','quickratio':'quick','cash':'cash','cashratio':'cash','icr':'icr','icratio':'icr','equity':'equity','sheqratio':'equity'}  # 排序检查用
+CASHFLOW_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/cashflow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s&order=%s%%7C2'    # 增加排序功能 &order=%s%%7C2
+CASHFLOW_ORDERBY = {'default':'cashflow2income','cashflow2income':'cashflow2income','cf_sales':'cashflow2income','cashflowrepay':'cashflowrepay','rateofreturn':'cashflowrepay','cashflow2netprofit':'cashflow2netprofit','cf_nm,':'cashflow2netprofit','cashflow2debt':'cashflow2debt','cf_liabilities':'cashflow2debt','cashflow':'cashflow','cashflowratio':'cashflow'}     # 排序检查用
+SHIBOR_TYPE = {'Shibor': 'Shibor数据', 'Quote': '报价数据', 'Tendency': 'Shibor均值数据',
+               'LPR': 'LPR数据', 'LPR_Tendency': 'LPR均值数据'}
 SHIBOR_DATA_URL = '%s%s/shibor/web/html/%s?nameNew=Historical_%s_Data_%s.xls&downLoadPath=data&nameOld=%s%s.xls&shiborSrc=http://www.shibor.org/shibor/'
-ALL_STOCK_BASICS_FILE = '%s%s/static/all.csv'%(P_TYPE['http'], DOMAINS['oss'])
-ALL_CAL_FILE = '%s%s/static/calAll.csv'%(P_TYPE['http'], DOMAINS['oss'])
+ALL_STOCK_BASICS_FILE = '%s%s/static/all.csv' % (P_TYPE['http'], DOMAINS['oss'])
+ALL_CAL_FILE = '%s%s/static/calAll.csv' % (P_TYPE['http'], DOMAINS['oss'])
 SINA_CONCEPTS_INDEX_URL = '%smoney.%s/q/view/%s?param=class'
 SINA_INDUSTRY_INDEX_URL = '%s%s/q/view/%s'
 SINA_DATA_DETAIL_URL = '%s%s/quotes_service/api/%s/Market_Center.getHQNodeData?page=1&num=1000&sort=symbol&asc=1&node=%s&symbol=&_s_r_a=page'
@@ -92,7 +99,7 @@ HS300_CLASSIFY_URL_HTTP = '%s%s/%s/%s'
 HIST_FQ_URL = '%s%s/corp/go.php/vMS_FuQuanMarketHistory/stockid/%s.phtml?year=%s&jidu=%s'
 HIST_INDEX_URL = '%s%s/corp/go.php/vMS_MarketHistory/stockid/%s/type/S.phtml?year=%s&jidu=%s'
 HIST_FQ_FACTOR_URL = '%s%s/api/json.php/BasicStockSrv.getStockFuQuanData?symbol=%s&type=hfq'
-INDEX_HQ_URL = '''%shq.%s/rn=xppzh&list=sh000001,sh000002,sh000003,sh000008,sh000009,sh000010,sh000011,sh000012,sh000016,sh000017,sh000300,sz399001,sz399002,sz399003,sz399004,sz399005,sz399006,sz399100,sz399101,sz399106,sz399107,sz399108,sz399333,sz399606'''
+INDEX_HQ_URL = '''%shq.%s/rn=xppzh&list=sh000001,sh000002,sh000003,sh000008,sh000009,sh000010,sh000011,sh000012,sh000016,sh000017,sh000300,sh000905,sz399001,sz399002,sz399003,sz399004,sz399005,sz399006,sz399008,sz399100,sz399101,sz399106,sz399107,sz399108,sz399333,sz399606'''
 SSEQ_CQ_REF_URL = '%s%s/assortment/stock/list/name'
 ALL_STK_URL = '%s%s/all.csv'
 SINA_DD = '%s%s/quotes_service/view/%s?symbol=%s&num=60&page=1&sort=ticktime&asc=0&volume=%s&amount=0&type=0&day=%s'
@@ -103,17 +110,17 @@ BOXOFFICE_MONTH = '%s%s/%s/getMonthBox?sdate=%s'
 BOXOFFICE_CBD = '%s%s/%s/getCBD?pIndex=%s&dt=%s'
 SHIBOR_COLS = ['date', 'ON', '1W', '2W', '1M', '3M', '6M', '9M', '1Y']
 QUOTE_COLS = ['date', 'bank', 'ON_B', 'ON_A', '1W_B', '1W_A', '2W_B', '2W_A', '1M_B', '1M_A',
-                    '3M_B', '3M_A', '6M_B', '6M_A', '9M_B', '9M_A', '1Y_B', '1Y_A']
-SHIBOR_MA_COLS = ['date', 'ON_5', 'ON_10', 'ON_20', '1W_5', '1W_10', '1W_20','2W_5', '2W_10', '2W_20',
+              '3M_B', '3M_A', '6M_B', '6M_A', '9M_B', '9M_A', '1Y_B', '1Y_A']
+SHIBOR_MA_COLS = ['date', 'ON_5', 'ON_10', 'ON_20', '1W_5', '1W_10', '1W_20', '2W_5', '2W_10', '2W_20',
                   '1M_5', '1M_10', '1M_20', '3M_5', '3M_10', '3M_20', '6M_5', '6M_10', '6M_20',
-                  '9M_5', '9M_10', '9M_20','1Y_5', '1Y_10', '1Y_20']
+                  '9M_5', '9M_10', '9M_20', '1Y_5', '1Y_10', '1Y_20']
 LPR_COLS = ['date', '1Y']
 LPR_MA_COLS = ['date', '1Y_5', '1Y_10', '1Y_20']
 INDEX_HEADER = 'code,name,open,preclose,close,high,low,0,0,volume,amount,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,d,c,3\n'
 INDEX_COLS = ['code', 'name', 'change', 'open', 'preclose', 'close', 'high', 'low', 'volume', 'amount']
 HIST_FQ_COLS = ['date', 'open', 'high', 'close', 'low', 'volume', 'amount', 'factor']
 SINA_DD_COLS = ['code', 'name', 'time', 'price', 'volume', 'preprice', 'type']
-HIST_FQ_FACTOR_COLS = ['code','value']
+HIST_FQ_FACTOR_COLS = ['code', 'value']
 DATA_GETTING_TIPS = '[Getting data:]'
 DATA_GETTING_FLAG = '#'
 DATA_ROWS_TIPS = '%s rows data found.Please wait for a moment.'
@@ -128,33 +135,49 @@ TOKEN_ERR_MSG = '请设置通联数据接口的token凭证码'
 BOX_INPUT_ERR_MSG = '请输入YYYY-MM格式的年月数据'
 
 import sys
+
 PY3 = (sys.version_info[0] >= 3)
+
+
 def _write_head():
     sys.stdout.write(DATA_GETTING_TIPS)
     sys.stdout.flush()
 
+
 def _write_console():
     sys.stdout.write(DATA_GETTING_FLAG)
     sys.stdout.flush()
-    
+
+
 def _write_tips(tip):
-    sys.stdout.write(DATA_ROWS_TIPS%tip)
+    sys.stdout.write(DATA_ROWS_TIPS % tip)
     sys.stdout.flush()
+
 
 def _write_msg(msg):
     sys.stdout.write(msg)
     sys.stdout.flush()
-    
+
+
 def _check_input(year, quarter):
-    if isinstance(year, str) or year < 1989 :
+    if isinstance(year, str) or year < 1989:
         raise TypeError(DATE_CHK_MSG)
     elif quarter is None or isinstance(quarter, str) or quarter not in [1, 2, 3, 4]:
         raise TypeError(DATE_CHK_Q_MSG)
     else:
         return True
-    
+
+
 def _check_lhb_input(last):
     if last not in [5, 10, 30, 60]:
         raise TypeError(LHB_MSG)
     else:
         return True
+
+# 增加排序检查
+def _check_orderby(orderby, orderitems):
+    orderby = orderby.lower()
+    if orderby in orderitems:
+        return orderitems[orderby]
+    else:
+        return orderitems['default']
