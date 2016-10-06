6b1ae5ede0ff825b8b4f9b921bf6ffb51d0325c9
jimmy <jimmysoa@sina.cn>
2016-10-06 19:40:17 +0800

    update for trading utils

--- tushare/trader/utils.py
+++ tushare/trader/utils.py
@@ -2,15 +2,13 @@
 # -*- coding:utf-8 -*- 
 
 '''
-Created on 2016年9月1日
+Created on 2016年10月1日
 @author: Jimmy Liu
 @group : waditu
 @contact: jimmysoa@sina.cn
 '''
 
-import datetime
 import json
-import uuid
 import time
 import six
 
@@ -25,25 +23,3 @@ def get_jdata(txtdata):
     jsonobj = json.loads(txtdata)
     return jsonobj
         
-
-def json_load(path):
-    with open(path) as f:
-        return json.load(f)
-    
-    
-def str2num(num_str, convert_type='float'):
-    num = float(grep_comma(num_str))
-    return num if convert_type == 'float' else int(num)
-
-
-def grep_comma(num_str):
-    return num_str.replace(',', '')
-
-
-def get_mac():
-    return ("".join(c + "-" if i % 2 else c for i, c in enumerate(hex(
-        uuid.getnode())[2:].zfill(12)))[:-1]).upper()
-        
-
-if __name__ == '__main__':
-    print(json_load('D:\\waditu\\workspace\\tushare\\tushare\\trader\\csc.json'))
