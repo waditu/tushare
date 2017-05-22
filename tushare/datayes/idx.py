02cbe7b6fda33a97fcd044b39eea4b24c9ff28e6
Sheki Lyu <lvxueji@gmail.com>
2017-05-22 17:19:15 +0800

    remove redundant intoDate query string fix error

--- tushare/datayes/idx.py
+++ tushare/datayes/idx.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 """
 通联数据
 Created on 2015/08/24
@@ -14,14 +14,14 @@ from tushare.util.common import Client
 from tushare.util import upass as up
 
 class Idx():
-    
+
     def __init__(self, client=None):
         if client is None:
             self.client = Client(up.get_token())
         else:
             self.client = client
-            
-            
+
+
     def Idx(self, secID='', ticker='', field=''):
         """
             获取国内外指数的基本要素信息，包括指数名称、指数代码、发布机构、发布日期、基日、基点等。
@@ -34,8 +34,8 @@ class Idx():
         """
             获取国内外指数的成分构成情况，包括指数成分股名称、成分股代码、入选日期、剔除日期等。
         """
-        code, result = self.client.getData(vs.IDXCONS%(secID, ticker, intoDate, 
-                                                       intoDate, isNew, field))
+        code, result = self.client.getData(vs.IDXCONS%(secID, ticker, intoDate,
+                                                       isNew, field))
         return _ret_data(code, result)
 
 
@@ -46,5 +46,4 @@ def _ret_data(code, result):
         return df
     else:
         print(result)
-        return None    
-    
+        return None
