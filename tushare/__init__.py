0a405e5205e3084f5886b1920e9446a686df571b
li-yong <sunraise2005@gmail.com>
2018-03-18 19:12:08 +0800

    import get_security
    from tushare.stock.trading import (get_security)

--- tushare/__init__.py
+++ tushare/__init__.py
@@ -13,7 +13,7 @@ from tushare.stock.trading import (get_hist_data, get_tick_data,
                                    get_k_data, get_day_all,
                                    get_sina_dd, bar, tick,
                                    get_markets, quotes,
-                                   get_instrument, reset_instrument)
+                                   get_instrument, reset_instrument, get_security)
 
 """
 for trading data
